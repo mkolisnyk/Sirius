0449624945cb540adc0d21f2bf536a91683ca146
Myk Kolisnyk <kolesnik.nickolay@gmail.com>
2013-03-02 15:12:01 +0000

    Update publish.cmd

--- Publish/publish.cmd
+++ Publish/publish.cmd
@@ -1,4 +1,4 @@
-call mvn assembly:single package:upload -Dpackage.version=%1
+call mvn assembly:single assembly:upload -Dpackage.version=%1
 REM cmd /C .\..\Utils\GCUpload.exe -u:kolesnik.nickolay@gmail.com -p:%GC_PASS% -s:"Publishing %1 release" -f:target\Sirius-%1-release.zip -n:sirius-platform
 
 echo "Publishing Ruby client"
