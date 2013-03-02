fae22b8ef81595f2cbc84bead36062c5d92f0d87
mkolisnyk <kolesnik.nickolay@gmail.com>
2013-03-02 16:58:00 +0000

    Gem and nuget packaging was removed from the batch

--- Publish/publish.cmd
+++ Publish/publish.cmd
@@ -1,11 +1,1 @@
 call mvn assembly:single gcu:upload -Dpackage.version=%1
-REM cmd /C .\..\Utils\GCUpload.exe -u:kolesnik.nickolay@gmail.com -p:%GC_PASS% -s:"Publishing %1 release" -f:target\Sirius-%1-release.zip -n:sirius-platform
-
-echo "Publishing Ruby client"
-
-for /f %%i in ('dir /B Ruby-Client\*.gem') DO cmd /C gem push Ruby-Client\%%i
-
-echo "Publishing C# client"
-
-.\..\Utils\NuGet.exe Update -self
-for /f %%i in ('dir /B CSharp-Client\*.nupkg') DO cmd /C .\..\Utils\NuGet.exe Push CSharp-Client\%%i
