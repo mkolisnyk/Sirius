d89153efd08d14a9f81ae1c839224b4d7976e6c3
mkolisnyk <kolesnik.nickolay@gmail.com>
2013-04-03 23:54:18 +0100

    CSharp client batches were updated with ability to publish packages

--- SiriusCSharp.Client/SiriusClient/Build.cmd
+++ SiriusCSharp.Client/SiriusClient/Build.cmd
@@ -1,2 +1,19 @@
 @echo off
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe BuildScript\Build.msbuild /p:ProductVersion=%1
+
+IF '%1' == '' GOTO USAGE
+
+echo "Building project for version number: %1"
+SET TARGETS=Build 
+
+IF '%2' == '' GOTO RUN
+
+echo "Setting custom target list to: %2"
+SET TARGETS=%2
+
+:RUN
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe BuildScript\Build.msbuild /p:ProductVersion=%1 /t:%TARGETS%
+GOTO END
+
+:USAGE
+
+:END
