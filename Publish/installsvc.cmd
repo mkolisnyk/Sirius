5343b020366f84d034b105ee29f3b737477110a0
mkolisnyk <kolesnik.nickolay@gmail.com>
2013-04-26 01:15:20 +0100

    Changes to Sirius server deploy batch

--- Publish/installsvc.cmd
+++ Publish/installsvc.cmd
@@ -1,4 +1,8 @@
-GOTO PUBLISH
+echo "Stop service"
+
+c:\windows\system32\sc.exe stop Sirius 4:4:4 "Planned update"
+instsrv Sirius REMOVE
+
 echo "Creating folder structure"
 
 DEL /Q /S .\Server\*.*
@@ -23,18 +27,22 @@ echo "Preparing service installation"
 
 copy ..\Utils\winrun4j\winrun4j.exe .\Server\sirius.exe
 copy .\sirius.ico .\server\sirius.ico
+copy .\sirius.ini .\server\sirius.ini
+echo working.directory=%CD%\Server >> .\server\sirius.ini
 
 cd .\Server
 ..\Utils\winrun4j\RCEDIT.exe /I sirius.exe sirius.ico
+..\Utils\winrun4j\RCEDIT.exe /A sirius.exe sirius.ico
 cd ..
 
 echo "Registering service"
 
-c:\windows\system32\sc.exe create Sirius type= share start= auto error= ignore binPath= D:\Work\SiriusDev\Sirius\Publish\Server\sirius.exe 
-
-c:\windows\system32\sc.exe config Sirius DisplayName= "Sirius Test Automation Platform Server v%1" obj=%SIRIUS_USER% password=%SIRIUS_PASSWORD%
+instsrv Sirius "%ProgramFiles%\Windows Resource Kits\Tools\srvany.exe" -a %SIRIUS_USER% -p %SIRIUS_PASSWORD%
+reg add HKLM\SYSTEM\CurrentControlSet\Services\Sirius /v Description /t REG_SZ /d "Sirius Test Automation Platform Server v%1" /f
+reg add HKLM\SYSTEM\CurrentControlSet\Services\Sirius\Parameters /f
+reg add HKLM\SYSTEM\CurrentControlSet\Services\Sirius\Parameters /v AppDirectory /t REG_SZ /d %CD%\Server /f
+reg add HKLM\SYSTEM\CurrentControlSet\Services\Sirius\Parameters /v Application /t REG_SZ /d %CD%\Server\sirius.exe /f
 
 echo "Restarting service"
 
-c:\windows\system32\sc.exe stop Sirius 4:4:4 "Planned update"
 c:\windows\system32\sc.exe start Sirius
