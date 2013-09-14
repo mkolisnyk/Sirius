781e38b0bd1d617c7c7a1899fe6e9452b865ef17
mkolisnyk <kolesnik.nickolay@gmail.com>
2013-09-14 16:47:30 +0100

    Batch for traceability matrix update was added

---
+++ Publish/update_trace_matrix.cmd
@@ -0,0 +1,16 @@
+echo "Creating folder structure"
+
+echo "Building Java client modules"
+
+cd ..
+call mvn -f sirius.utils.retriever/pom.xml -Dpackage.version=%1 -Dversion=%1 -DpomFile=pom_local.xml -Dpackaging=jar -DgeneratePom=true clean package
+java -jar sirius.utils.retriever/target/sirius.utils.retriever-%1.jar > ../wiki/Traceability-Matrix.md
+
+cd ../wiki/
+git pull
+git commit -a -m "Traceability matrix update. Version: %1"
+git push
+
+cd ../sirius/
+
+cd .\Publish
