055e66fbabff45e622970c2df2dd683eb5623b34
mkolisnyk <kolesnik.nickolay@gmail.com>
2013-12-01 18:21:24 +0000

    Ruby Win32 client was updated

--- sirius-ruby-client-win32/lib/sirius/win32/classes/controls/ListBox.rb
+++ sirius-ruby-client-win32/lib/sirius/win32/classes/controls/ListBox.rb
@@ -7,6 +7,188 @@ module Sirius
             def initialize(locator, parent = nil)
               super(locator, parent)
             end
+
+            # SYNOPSIS
+            #   addSelectionByIndex(parameters)
+            #
+            # ARGS
+            #   parameters      AddSelectionByIndex - {http://controls.classes.win32.server.sirius.org/}addSelectionByIndex
+            #
+            # RETURNS
+            #   parameters      AddSelectionByIndexResponse - {http://controls.classes.win32.server.sirius.org/}addSelectionByIndexResponse
+            #
+            def add_selection_by_index
+              @client.core.listbox.addSelectionByIndex(parameters)
+            end
+
+            # SYNOPSIS
+            #   addSelectionByName(parameters)
+            #
+            # ARGS
+            #   parameters      AddSelectionByName - {http://controls.classes.win32.server.sirius.org/}addSelectionByName
+            #
+            # RETURNS
+            #   parameters      AddSelectionByNameResponse - {http://controls.classes.win32.server.sirius.org/}addSelectionByNameResponse
+            #
+            def add_selection_by_name
+              @client.core.listbox.addSelectionByName(parameters)
+            end
+
+            # SYNOPSIS
+            #   getItemsCount(parameters)
+            #
+            # ARGS
+            #   parameters      GetItemsCount - {http://controls.classes.win32.server.sirius.org/}getItemsCount
+            #
+            # RETURNS
+            #   parameters      GetItemsCountResponse - {http://controls.classes.win32.server.sirius.org/}getItemsCountResponse
+            #
+            def get_items_count
+              @client.core.listbox.getItemsCount(parameters)
+            end
+
+            # SYNOPSIS
+            #   getSelectedItems(parameters)
+            #
+            # ARGS
+            #   parameters      GetSelectedItems - {http://controls.classes.win32.server.sirius.org/}getSelectedItems
+            #
+            # RETURNS
+            #   parameters      GetSelectedItemsResponse - {http://controls.classes.win32.server.sirius.org/}getSelectedItemsResponse
+            #
+            def get_selected_items
+              @client.core.listbox.getSelectedItems(parameters)
+            end
+
+            # SYNOPSIS
+            #   selectByIndex(parameters)
+            #
+            # ARGS
+            #   parameters      SelectByIndex - {http://controls.classes.win32.server.sirius.org/}selectByIndex
+            #
+            # RETURNS
+            #   parameters      SelectByIndexResponse - {http://controls.classes.win32.server.sirius.org/}selectByIndexResponse
+            #
+            def select_by_index
+              @client.core.listbox.selectByIndex(parameters)
+            end
+
+            # SYNOPSIS
+            #   getEndpoint(parameters)
+            #
+            # ARGS
+            #   parameters      GetEndpoint - {http://controls.classes.win32.server.sirius.org/}getEndpoint
+            #
+            # RETURNS
+            #   parameters      GetEndpointResponse - {http://controls.classes.win32.server.sirius.org/}getEndpointResponse
+            #
+            def get_endpoint
+              @client.core.listbox.getEndpoint(parameters)
+            end
+
+            # SYNOPSIS
+            #   removeSelectionByIndex(parameters)
+            #
+            # ARGS
+            #   parameters      RemoveSelectionByIndex - {http://controls.classes.win32.server.sirius.org/}removeSelectionByIndex
+            #
+            # RETURNS
+            #   parameters      RemoveSelectionByIndexResponse - {http://controls.classes.win32.server.sirius.org/}removeSelectionByIndexResponse
+            #
+            def remove_selection_by_index
+              @client.core.listbox.removeSelectionByIndex(parameters)
+            end
+
+            # SYNOPSIS
+            #   removeSelectionByName(parameters)
+            #
+            # ARGS
+            #   parameters      RemoveSelectionByName - {http://controls.classes.win32.server.sirius.org/}removeSelectionByName
+            #
+            # RETURNS
+            #   parameters      RemoveSelectionByNameResponse - {http://controls.classes.win32.server.sirius.org/}removeSelectionByNameResponse
+            #
+            def remove_selection_by_name
+              @client.core.listbox.removeSelectionByName(parameters)
+            end
+
+            # SYNOPSIS
+            #   deselectAll(parameters)
+            #
+            # ARGS
+            #   parameters      DeselectAll - {http://controls.classes.win32.server.sirius.org/}deselectAll
+            #
+            # RETURNS
+            #   parameters      DeselectAllResponse - {http://controls.classes.win32.server.sirius.org/}deselectAllResponse
+            #
+            def deselect_all
+              @client.core.listbox.deselectAll(parameters)
+            end
+
+            # SYNOPSIS
+            #   getItemNames(parameters)
+            #
+            # ARGS
+            #   parameters      GetItemNames - {http://controls.classes.win32.server.sirius.org/}getItemNames
+            #
+            # RETURNS
+            #   parameters      GetItemNamesResponse - {http://controls.classes.win32.server.sirius.org/}getItemNamesResponse
+            #
+            def get_item_names
+              @client.core.listbox.getItemNames(parameters)
+            end
+
+            # SYNOPSIS
+            #   selectByName(parameters)
+            #
+            # ARGS
+            #   parameters      SelectByName - {http://controls.classes.win32.server.sirius.org/}selectByName
+            #
+            # RETURNS
+            #   parameters      SelectByNameResponse - {http://controls.classes.win32.server.sirius.org/}selectByNameResponse
+            #
+            def select_by_name
+              @client.core.listbox.selectByName(parameters)
+            end
+
+            # SYNOPSIS
+            #   selectAll(parameters)
+            #
+            # ARGS
+            #   parameters      SelectAll - {http://controls.classes.win32.server.sirius.org/}selectAll
+            #
+            # RETURNS
+            #   parameters      SelectAllResponse - {http://controls.classes.win32.server.sirius.org/}selectAllResponse
+            #
+            def select_all
+              @client.core.listbox.selectAll(parameters)
+            end
+
+            # SYNOPSIS
+            #   getSelectedIndex(parameters)
+            #
+            # ARGS
+            #   parameters      GetSelectedIndex - {http://controls.classes.win32.server.sirius.org/}getSelectedIndex
+            #
+            # RETURNS
+            #   parameters      GetSelectedIndexResponse - {http://controls.classes.win32.server.sirius.org/}getSelectedIndexResponse
+            #
+            def get_selected_index
+              @client.core.listbox.getSelectedIndex(parameters)
+            end
+
+            # SYNOPSIS
+            #   getSelectedItem(parameters)
+            #
+            # ARGS
+            #   parameters      GetSelectedItem - {http://controls.classes.win32.server.sirius.org/}getSelectedItem
+            #
+            # RETURNS
+            #   parameters      GetSelectedItemResponse - {http://controls.classes.win32.server.sirius.org/}getSelectedItemResponse
+            #
+            def get_selected_item
+              @client.core.listbox.getSelectedItem(parameters)
+            end
           end
         end
       end
--- sirius-ruby-client-win32/lib/sirius/win32/classes/controls/Spin.rb
+++ sirius-ruby-client-win32/lib/sirius/win32/classes/controls/Spin.rb
@@ -7,6 +7,136 @@ module Sirius
             def initialize(locator, parent = nil)
               super(locator, parent)
             end
+
+            # SYNOPSIS
+            #   getLowerBound(parameters)
+            #
+            # ARGS
+            #   parameters      GetLowerBound - {http://controls.classes.win32.server.sirius.org/}getLowerBound
+            #
+            # RETURNS
+            #   parameters      GetLowerBoundResponse - {http://controls.classes.win32.server.sirius.org/}getLowerBoundResponse
+            #
+            def get_lower_bound
+              @client.core.spin.getLowerBound(parameters)
+            end
+
+            # SYNOPSIS
+            #   getUpperBound(parameters)
+            #
+            # ARGS
+            #   parameters      GetUpperBound - {http://controls.classes.win32.server.sirius.org/}getUpperBound
+            #
+            # RETURNS
+            #   parameters      GetUpperBoundResponse - {http://controls.classes.win32.server.sirius.org/}getUpperBoundResponse
+            #
+            def get_upper_bound
+              @client.core.spin.getUpperBound(parameters)
+            end
+
+            # SYNOPSIS
+            #   getSmallStep(parameters)
+            #
+            # ARGS
+            #   parameters      GetSmallStep - {http://controls.classes.win32.server.sirius.org/}getSmallStep
+            #
+            # RETURNS
+            #   parameters      GetSmallStepResponse - {http://controls.classes.win32.server.sirius.org/}getSmallStepResponse
+            #
+            def get_small_step
+              @client.core.spin.getSmallStep(parameters)
+            end
+
+            # SYNOPSIS
+            #   getLargeStep(parameters)
+            #
+            # ARGS
+            #   parameters      GetLargeStep - {http://controls.classes.win32.server.sirius.org/}getLargeStep
+            #
+            # RETURNS
+            #   parameters      GetLargeStepResponse - {http://controls.classes.win32.server.sirius.org/}getLargeStepResponse
+            #
+            def get_large_step
+              @client.core.spin.getLargeStep(parameters)
+            end
+
+            # SYNOPSIS
+            #   increment(parameters)
+            #
+            # ARGS
+            #   parameters      Increment - {http://controls.classes.win32.server.sirius.org/}increment
+            #
+            # RETURNS
+            #   parameters      IncrementResponse - {http://controls.classes.win32.server.sirius.org/}incrementResponse
+            #
+            def increment
+              @client.core.spin.increment(parameters)
+            end
+
+            # SYNOPSIS
+            #   getText(parameters)
+            #
+            # ARGS
+            #   parameters      GetText - {http://controls.classes.win32.server.sirius.org/}getText
+            #
+            # RETURNS
+            #   parameters      GetTextResponse - {http://controls.classes.win32.server.sirius.org/}getTextResponse
+            #
+            def get_text
+              @client.core.spin.getText(parameters)
+            end
+
+            # SYNOPSIS
+            #   setText(parameters)
+            #
+            # ARGS
+            #   parameters      SetText - {http://controls.classes.win32.server.sirius.org/}setText
+            #
+            # RETURNS
+            #   parameters      SetTextResponse - {http://controls.classes.win32.server.sirius.org/}setTextResponse
+            #
+            def set_text
+              @client.core.spin.setText(parameters)
+            end
+
+            # SYNOPSIS
+            #   decrement(parameters)
+            #
+            # ARGS
+            #   parameters      Decrement - {http://controls.classes.win32.server.sirius.org/}decrement
+            #
+            # RETURNS
+            #   parameters      DecrementResponse - {http://controls.classes.win32.server.sirius.org/}decrementResponse
+            #
+            def decrement
+              @client.core.spin.decrement(parameters)
+            end
+
+            # SYNOPSIS
+            #   getPosition(parameters)
+            #
+            # ARGS
+            #   parameters      GetPosition - {http://controls.classes.win32.server.sirius.org/}getPosition
+            #
+            # RETURNS
+            #   parameters      GetPositionResponse - {http://controls.classes.win32.server.sirius.org/}getPositionResponse
+            #
+            def get_position
+              @client.core.spin.getPosition(parameters)
+            end
+
+            # SYNOPSIS
+            #   setPosition(parameters)
+            #
+            # ARGS
+            #   parameters      SetPosition - {http://controls.classes.win32.server.sirius.org/}setPosition
+            #
+            # RETURNS
+            #   parameters      SetPositionResponse - {http://controls.classes.win32.server.sirius.org/}setPositionResponse
+            #
+            def set_position
+              @client.core.spin.setPosition(parameters)
+            end
           end
         end
       end
